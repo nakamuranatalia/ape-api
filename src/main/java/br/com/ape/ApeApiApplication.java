a9a634f36f7b20d762138762ce7e4ff9e31ae161
Naat <nakamura.natalia22@gmail.com>
2023-11-28 18:48:22 -0300

    init

---
+++ src/main/java/br/com/ape/ApeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ape/ApeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
