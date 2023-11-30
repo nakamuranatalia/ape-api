96fcac4d8bb1d4abb34be07daac232c954f3b07e
Naat <nakamura.natalia22@gmail.com>
2023-11-30 17:06:58 -0300

    Changing isSimian HTTP verb from get to post

--- src/main/java/br/com/ape/controller/ApeController.java
+++ src/main/java/br/com/ape/controller/ApeController.java
@@ -2,9 +2,11 @@ package br.com.ape.controller;
 
 import br.com.ape.service.ApeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/simian")
@@ -13,8 +15,8 @@ public class ApeController {
 
     private final ApeService service;
 
-    @GetMapping
-    public void isSimian(String [] dna){
+    @PostMapping
+    public void isSimian(@RequestBody String[] dna){
         service.isSimian(dna);
     }
 }
