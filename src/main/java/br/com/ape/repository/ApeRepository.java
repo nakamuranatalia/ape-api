b59e8121507ed87d6edcdf2a0e90f0a9113cad00
Naat <nakamura.natalia22@gmail.com>
2023-12-13 15:40:16 -0300

    Creating ApeRepository

---
+++ src/main/java/br/com/ape/repository/ApeRepository.java
@@ -0,0 +1,9 @@
+package br.com.ape.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.ape.model.Ape;
+
+@Repository
+public interface ApeRepository extends JpaRepository<Ape, Long> {
+}
