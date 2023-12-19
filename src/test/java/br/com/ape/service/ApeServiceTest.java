b9c7937af469e01cee5b86943901da5f7c6fb778
Naat <nakamura.natalia22@gmail.com>
2023-12-19 18:50:21 -0300

    Creating tests related to isValidDna method on ApeServiceTest

--- src/test/java/br/com/ape/service/ApeServiceTest.java
+++ src/test/java/br/com/ape/service/ApeServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.ape.service;
 
 import br.com.ape.repository.ApeRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,4 +16,56 @@ public class ApeServiceTest {
 
     @InjectMocks
     private ApeService service;
+
+    @Test
+    public void givenNonSquareDna_whenIsDnaValid_thenReturnFalse(){
+        //Arrange
+        String[] dna = new String[]{"ATTG", "ATTT"};
+        boolean expectedResult = false;
+
+        //Act
+        boolean result = service.isValidDna(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void givenNonDnaSequence_whenIsDnaValid_thenReturnFalse(){
+        //Arrange
+        String[] dna = new String[]{"ATTT", "LNVF"};
+        boolean expectedResult = false;
+
+        //Act
+        boolean result = service.isValidDna(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void givenInvalidDnaArray_whenIsDnaValid_thenReturnFalse(){
+        //Arrange
+        String[] dna = new String[]{"ATTG"};
+        boolean expectedResult = false;
+
+        //Act
+        boolean result = service.isValidDna(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void givenValidDna_whenIsDnaValid_thenReturnTrue(){
+        //Arrange
+        String[] dna = new String[]{"ATTG", "ATTT", "TGCA", "GCAT"};
+        boolean expectedResult = true;
+
+        //Act
+        boolean result = service.isValidDna(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
