f7d5934faa0988d42048efb4afc00fea43bb3cb4
Naat <nakamura.natalia22@gmail.com>
2023-12-19 17:00:58 -0300

    Creating a test class for ApeService

---
+++ src/test/java/br/com/ape/service/ApeServiceTest.java
@@ -0,0 +1,18 @@
+package br.com.ape.service;
+
+import br.com.ape.repository.ApeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ApeServiceTest {
+
+    @Mock
+    private ApeRepository repository;
+
+    @InjectMocks
+    private ApeService service;
+}
