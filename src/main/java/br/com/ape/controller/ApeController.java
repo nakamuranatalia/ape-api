b331ddc1ccfcd0608523269dcfd388d670792d74
Naat <nakamura.natalia22@gmail.com>
2023-12-21 16:35:44 -0300

    isSimian method returning the expected HTTP status on ApeController

--- src/main/java/br/com/ape/controller/ApeController.java
+++ src/main/java/br/com/ape/controller/ApeController.java
@@ -2,21 +2,28 @@ package br.com.ape.controller;
 
 import br.com.ape.service.ApeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/simian")
 @RequiredArgsConstructor
 public class ApeController {
 
     private final ApeService service;
 
     @PostMapping
-    public void isSimian(@RequestBody String[] dna){
-        service.isSimian(dna);
+    @RequestMapping("/simian")
+    public ResponseEntity<String> isSimian(@RequestBody String[] dna){
+        if(!service.isValidDna(dna)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Array format is incorrect");
+
+        if(service.isSimian(dna)){
+            return ResponseEntity.status(HttpStatus.OK).body("Informed DNA is simian");
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Informed DNA is human");
+        }
     }
 }
