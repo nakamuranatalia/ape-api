005e0600d1ddb06628e8fede2604f09369058e5e
Naat <nakamura.natalia22@gmail.com>
2023-12-21 16:33:24 -0300

    Adding tests related to IsSimian method on ApeServiceTest

--- src/test/java/br/com/ape/service/ApeServiceTest.java
+++ src/test/java/br/com/ape/service/ApeServiceTest.java
@@ -2,7 +2,6 @@ package br.com.ape.service;
 
 import br.com.ape.repository.ApeRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -51,6 +50,43 @@ public class ApeServiceTest {
     public void givenValidDna_whenIsDnaValid_thenReturnTrue(String[] dna, boolean expectedResult) {
         //Act
         boolean result = service.isValidDna(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    static Stream<Arguments> simianDna(){
+        return Stream.of(
+                Arguments.of(new String[]{"CATA", "ATCT", "CGTG", "AAAA"}, true),
+                Arguments.of(new String[]{"CATG", "ATCG", "CGTG", "AAAG"}, true),
+                Arguments.of(new String[]{"TATA", "ATCT", "CGTG", "AAAT"}, true),
+                Arguments.of(new String[]{"TATC", "ATCT", "CCAG", "CAAT"}, true),
+                Arguments.of(new String[]{"TATCA", "ATCTT", "ACTAA", "CATGG", "GATCG"}, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("simianDna")
+    public void givenSimianDna_whenIsSimian_returnTrue(String[] dna, boolean expectedResult){
+        //Act
+        boolean result = service.isSimian(dna);
+
+        //Assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    static Stream<Arguments> humanDna(){
+        return Stream.of(
+                Arguments.of(new String[]{"ATTG", "ATTT", "TGCA", "GCAT"}, false),
+                Arguments.of(new String[]{"ATTGTT", "ATTTCC", "TGCAAC", "GCATTC", "ATTGTA", "GGGTCC"}, false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("humanDna")
+    public void givenHumanDna_whenIsSimian_returnFalse(String[] dna, boolean expectedResult){
+        //Act
+        boolean result = service.isSimian(dna);
 
         //Assert
         Assertions.assertEquals(expectedResult, result);
