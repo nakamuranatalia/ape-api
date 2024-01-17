733dbe21e07f847ab0978467ed37d551d903ebfb
Naat <nakamura.natalia22@gmail.com>
2024-01-17 15:22:19 -0300

    Adding annotation to ApeApiApplicationTests and renaming application.properties used on tests.

--- src/test/java/br/com/ape/ApeApiApplicationTests.java
+++ src/test/java/br/com/ape/ApeApiApplicationTests.java
@@ -1,9 +1,14 @@
 package br.com.ape;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = ApeApiApplication.class)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 class ApeApiApplicationTests {
 
 	@Test
