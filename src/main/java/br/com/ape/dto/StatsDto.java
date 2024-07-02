fc91f74f40aa4574e3e38ffc38547369c0d9da28
Naat <nakamura.natalia22@gmail.com>
2024-07-02 18:18:44 -0300

    Making StatsDto serializable

--- src/main/java/br/com/ape/dto/StatsDto.java
+++ src/main/java/br/com/ape/dto/StatsDto.java
@@ -3,9 +3,11 @@ package br.com.ape.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class StatsDto {
+public class StatsDto implements Serializable {
     private float countMutantDna;
     private float countHumanDna;
     private float ratio;
